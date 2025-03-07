7a3d0aa909210367b86ac6a1f078f77b2c7bede2
Bhargav-Ravinuthala <63846781+Bhargav-Ravinuthala@users.noreply.github.com>
2025-03-07 15:19:19 +0530

    Update BaseFooter.tsx

--- app/components/layout/BaseFooter.tsx
+++ app/components/layout/BaseFooter.tsx
@@ -1,1 +1,15 @@
+"use client";
+import { useTranslationContext } from "@/contexts/TranslationContext";
 
+const BaseFooter = () => {
+    const { _t } = useTranslationContext();
+    return (
+        <footer className="container py-10">
+            <p>
+                {/* Footer content removed */}
+            </p>
+        </footer>
+    );
+};
+
+export default BaseFooter;
